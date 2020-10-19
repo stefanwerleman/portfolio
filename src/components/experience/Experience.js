d3edd4630944702aaace366ad1fa9ce9e87ced3b
stefanwerleman <stefanwerleman@knights.ucf.edu>
2020-10-19 10:39:25 -0400

    finished experience page

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -12,6 +12,39 @@ class Experience extends React.Component {
                             <hr style={styles.mainLine} />
                         </Col>
                     </Row>
+                    <Row className="justify-content-md-center">
+                        <Col md="auto">
+                            <h2>
+                                <strong>
+                                    Car Wash Palace and Detail Center
+                                </strong>
+                            </h2>
+                        </Col>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <Col md="auto">
+                            <h5 style={{ color: "#39689b" }}>
+                                Detail Specialist and Sales Associate
+                            </h5>
+                        </Col>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <Col md="auto">
+                            <h5>
+                                <em>May 2017 - PRESENT</em>
+                            </h5>
+                        </Col>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <Col md="auto">
+                            <p>
+                                Currently work 30 hours a week. Perform auto
+                                detailing services on various vehicles and am
+                                responsible for the sales of detail sercives and
+                                products.
+                            </p>
+                        </Col>
+                    </Row>
                 </Container>
             </div>
         );
@@ -21,6 +54,7 @@ class Experience extends React.Component {
 const styles = {
     container: {
         paddingTop: "5.20%",
+        paddingBottom: "12.13%",
     },
     mainLine: {
         borderColor: "#FF1B1C",
