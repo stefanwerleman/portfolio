c847ad3c68a5818a789efff663a37e4a381e4a0d
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-02-23 20:54:11 -0500

    Starting new feature

---
+++ client/src/components/job/Job.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Job.css';
+
+import { Container, Row, Col } from 'react-bootstrap';
+
+class Job extends React.Component {
+	render() {
+		return (
+			<div id='job'>
+				<Container>Hello</Container>
+			</div>
+		);
+	}
+}
+
+export default Job;
